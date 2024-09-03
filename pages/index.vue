9c8a0324c9183c491e68ced8dd7ce7ec7b89cb79
zekor <zakariarezk0@gmail.com>
2024-09-03 09:51:01 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+const nuxtApp = useNuxtApp();
+
+const { data } = await useFetch("https://icanhazdadjoke.com/", {
+  headers: {
+    Accept: "application/json",
+  },
+  getCachedData(key) {
+   
+    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>Index page</h1>
+    <p>{{ data.joke }}</p>
+    <NuxtLink to="/about">To the about page</NuxtLink>
+  </div>
+</template>
